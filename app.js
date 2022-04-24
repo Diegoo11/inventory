7cb4d15e368142d45df76d8279a5f21e8028218b
Diegoo11 <ynoacamino@gmail.com>
2022-04-24 16:50:43 -0500

    add config de mongoose and new router home

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+
+const dev_db_url = '';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect( mongoDB, {useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MingiDB connection error:'))
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -21,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
