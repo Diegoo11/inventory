fa4863c3cc94d758140e8b47bbe9d6e9b59321ec
Diegoo11 <ynoacamino@gmail.com>
2022-04-24 19:06:54 -0500

    modelos para inviar a mongodb

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  category: {type: Schema.Types.ObjectId, ref: 'Category'},
+  cost: {type: Number, required: true},
+  isbn: {type: String, required: true},
+});
+
+ItemSchema.virtual('url').get(function () {
+  return '/catalog/item/' + this._id;
+});
+
+module.exports = mongoose.model('Item', ItemSchema)
