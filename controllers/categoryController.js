0badc566a9d41e2499d49c2363d293c991f4db22
Diegoo11 <ynoacamino@gmail.com>
2022-04-24 19:27:03 -0500

    edit categorycontroller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,11 +5,32 @@ const { body, validationResult } = require('express-validator');
 const async = require('async');
 
 exports.category_list = function(req, res, next) {
-
+  Category.find().exec(function(err, categories) {
+    if(err) {return next(err)}
+    res.render('category_list', {
+      title: 'Categories',
+      categories: categories
+    })
+  })
 };
 
 exports.category_detail = function(req, res, next) {
-
+  async.parallel({
+    category: function(callback) {
+      Category.findById(req.params.id).exec(callback)
+    },
+    category_items: function(callback) {
+      Item.find({ 'category': req.params.id }).exec(callback)
+    }
+  }, function(err, results) {
+    if(err) {return next(err)}
+    if(results === null) {
+      const err = new Error('Category not found');
+      err.status = 404;
+      return next(err)
+    }
+    res.render('')
+  })
 };
 
 exports.category_create_get = function(req, res, next) {
