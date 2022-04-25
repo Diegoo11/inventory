ec47b87c44ef0c2ec6a7c0c94919c1efb325b5c6
Diegoo11 <ynoacamino@gmail.com>
2022-04-25 12:50:15 -0500

    all controllers completed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,30 +29,129 @@ exports.category_detail = function(req, res, next) {
       err.status = 404;
       return next(err)
     }
-    res.render('')
+    res.render('category_detail', {
+      title: 'Category Detail',
+      category: results.category,
+      category_items: results.category_items
+    })
   })
 };
 
 exports.category_create_get = function(req, res, next) {
-
+  res.render('category_create', {title: 'Category Create'})
 };
 
-exports.category_create_post = function(req, res, next) {
+exports.category_create_post = [
+  body('name').trim().isLength({min:1}).escape(),
+  body('description').trim().isLength({min:1}).escape(),
 
-};
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description
+    })
+
+    if(!errors.isEmpty()) {
+      res.render('category_create', {
+        title: 'Category Create',
+        category: category,
+        errors: errors.array()
+      })
+    }
+    else {
+      category.save(function (err) {
+        if(err) {return next(err)}
+        res.redirect(category.url)
+      })
+    }
+  }
+]
 
 exports.category_upgrate_get = function(req, res, next) {
-
+  Category.findById(req.params.id).exec(function (err, category) {
+    if(err) {return next(err)}
+    if(category === null) {
+      const err = new Error('Category not found');
+      err.status = 404;
+      return next(err)
+    }
+    res.render('category_create', {title: 'Category Update', category: category})
+  })
 };
 
-exports.category_upgrate_post = function(req, res, next) {
+exports.category_upgrate_post = [
+  body('name').trim().isLength({min:1}).escape(),
+  body('description').trim().isLength({min:1}).escape(),
 
-};
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.name
+    })
+
+    if(!errors.isEmpty()) {
+      res.render('category_create', {
+        title: 'Category Update',
+        category: category,
+        errors: errors.array()
+      })
+    }
+    else {
+      Category.findByIdAndUpdate(req.params.id, category, {}, function(err, thecategory) {
+        if(err) {return next(err)}
+        res.redirect(thecategory.url)
+      })
+    }
+  }
+]
 
 exports.category_delete_get = function(req, res, next) {
-
+  async.parallel({
+    category: function(callback) {
+      Category.findById(req.params.id).exec(callback);
+    },
+    category_items: function(callback) {
+      Item.find({ 'category': req.params.id }).exec(callback)
+    }
+  }, function(err, results) {
+    if(err) {return next(err)};
+    if(results.category == null) {
+      res.redirect('/catalog/categories');
+    }
+    res.render('category_delete', {
+      title: 'Category Delete: ' + results.category.name,
+      category: results.category,
+      category_items: results.category_items
+    })
+  })
 };
 
 exports.category_delete_post = function(req, res, next) {
-
+  async.parallel({
+    category: function(callback) {
+      Category.findById(req.body.categoryid)
+    },
+    category_items: function(callback) {
+      Item.find({'category': req.params.id}).exec(callback)
+    }
+  }, function(err, results) {
+    if(err) {return next(err)}
+    if(results.category_items.length > 0){
+      res.render('category_delete', {
+        title: 'Category Delete: ' + results.category.name,
+        category: results.category,
+        category_items: results.category_items
+      })
+      return
+    }
+    else {
+      Category.findByIdAndRemove(req.body.categoryid, function deleteCategory(err) {
+        if(err) {return next(err)}
+        res.redirect('/catalog/categories')
+      })
+    }
+  })
 };
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -9,33 +9,149 @@ exports.index = function(req, res, next) {
 };
 
 exports.item_list = function(req, res, next) {
-
+  Item.find().exec(function(err, item) {
+    if(err) {return next(err)};
+    res.render('item_list', {
+      title: 'Item List',
+      item: item
+    })
+  })
 };
 
 exports.item_detail = function(req, res, next) {
-
+  Item.findById(req.params.id).populate('category').exec(function (err, item) {
+    if(err) {return next(err)};
+    if(item === null) {
+      const err = new Error('Item not found');
+      err.status = 404;
+      return next(err);
+    };
+    res.render('item_detail', {
+      title: item.category.name,
+      item: item
+    });
+  });
 };
 
 exports.item_create_get = function(req, res, next) {
-
+  Category.find().exec(function(err, category) {
+    if(err) {return next(err)}
+    res.render('item_create', {
+      title: 'Create Item',
+      category: category
+    })
+  })
 };
 
-exports.item_create_post = function(req, res, next) {
+exports.item_create_post = [
+  body('name').trim().isLength({min:1}).escape(),
+  body('description').trim().isLength({min:1}).escape(),
+  body('cost').trim().isLength({min:1}).escape(),
+  body('isbn').trim().isLength({min:1}).escape(),
 
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      cost: req.body.cost,
+      isbn: req.body.isbn,
+      category: req.body.category
+    })
+
+    if(!errors.isEmpty()) {
+      Category.find().exec(function(err, category) {
+        if(err) {return next(err)}
+        res.render('item_create', {
+          title: 'Create Item',
+          item: item,
+          category: category,
+          errors: errors.array()
+        })
+      })
+      return
+    }
+    else {
+      item.save(function(err) {
+        if(err) {return next(err)}
+        res.redirect(item.url)
+      })
+    }
+  }
+]
+
+exports.item_update_get = function(req, res, next) {
+  Item.findById(req.params.id).populate('category').exec(function (err, item) {
+    if(err) {return next(err)};
+    if(item === null) {
+      const err = new Error('Item not found');
+      err.status = 404;
+      return next(err);
+    };
+    res.render('item_create', {
+      title: 'Item Update',
+      item: item
+    })
+  })
 };
+
+exports.item_update_post = [
+  body('name').trim().isLength({min:1}).escape(),
+  body('description').trim().isLength({min:1}).escape(),
+  body('cost').trim().isLength({min:1}).escape(),
+  body('isbn').trim().isLength({min:1}).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      cost: req.body.cost,
+      isbn: req.body.isbn,
+      category: req.body.category
+    })
+
+    if(!errors.isEmpty()) {
+      Category.find().exec(function(err, category) {
+        if(err) {return next(err)}
+        res.render('item_update', {
+          title: 'Create Item',
+          item: item,
+          category: category,
+          errors: errors.array()
+        })
+      })
+      return
+    }
+    else {
+      Item.findByIdAndUpdate(req.params.id, item, {}, function(err, theitem) {
+        if(err) {return next(err)};
+        res.redirect(theitem.url);
+      })
+    }
+  }
+]
 
 exports.item_delete_get = function(req, res, next) {
-
+  Item.findById(req.params.id).populate('category').exec(function(err, item) {
+    if(err) {return next(err)};
+    if(item === null) {
+      const err = new Error('Item not found');
+      err.status = 404;
+      return next(err);
+    };
+    res.render('item_delete', {
+      title: 'Delete Item: ' + item.name,
+      item: item
+    })
+  })
 };
 
 exports.item_delete_post = function(req, res, next) {
-
-};
-
-exports.item_update_get = function(req, res, next) {
-
-};
-
-exports.item_update_post = function(req, res, next) {
-
+  Item.findByIdAndRemove(req.body.itemid, function(err) {
+    if(err) {return next(err)};
+    res.redirect('/catalog/items')
+  })
 };
