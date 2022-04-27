baa1ec573577c2d039c757640d567ba9b024b07a
Diegoo11 <ynoacamino@gmail.com>
2022-04-27 09:33:34 -0500

    add route contact

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,10 @@ const category_controller = require('../controllers/categoryController')
 
 router.get('/', item_controller.index);
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact')
+});
+
 router.get('/items', item_controller.item_list);
 
 router.get('/item/create', item_controller.item_create_get);
