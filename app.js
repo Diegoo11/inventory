4a90f92a3b6737e7481dc90b53d0f5b2792963e4
Diegoo11 <ynoacamino@gmail.com>
2022-04-27 09:32:04 -0500

    add module comprress()

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const homeRouter = require('./routes/home');
 
+const compression = require('compression')
+
 var app = express();
 
 // view engine setup
@@ -25,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
